1f78111e342a6319e9923b308884ad025c4e6a85
Abhey gupta <abheyygupta396@gmail.com>
2021-06-11 22:11:44 +0530

    working on header ui

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Router, Route} from 'react-router-dom';
-import {Redirect, Switch} from "react-router";
+import { Router, Route } from 'react-router-dom';
+import { Redirect, Switch } from "react-router";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
-import {history} from "./managers/history";
+import { connect } from "react-redux";
+import { Login, SignUp } from "./modules";
+import { history } from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
+import navigationBar from "./modules/explorer/navigationBar"
+
+
 
 class Routes extends BaseComponent {
 
@@ -20,9 +23,9 @@ class Routes extends BaseComponent {
             <MuiThemeProvider muiTheme={getMuiTheme()}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path={'/'} component={Login}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
-                        <Redirect exact from='*' to="/"/>
+                        <Route exact path={'/sign-up'} component={SignUp} />
+                        <Route exact path={'/'} component={navigationBar} />
+                        <Redirect exact from='*' to="/" />
                     </Switch>
                 </Router>
             </MuiThemeProvider>);
@@ -30,6 +33,8 @@ class Routes extends BaseComponent {
 }
 
 const mapStateToProps = (state) => {
-    return {user: state.user}
+    return { user: state.user }
 };
 export default connect(mapStateToProps)(Routes);
+
+{/*  */ }
