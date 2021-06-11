04cef0baf963417c8612c4095b7f1407ad1abac6
aditya dubey <adityad@leewayhertz.com>
2021-06-11 23:38:32 +0530

    UI for blockchain details on dashboard

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Router, Route} from 'react-router-dom';
-import {Redirect, Switch} from "react-router";
+import { Router, Route } from 'react-router-dom';
+import { Redirect, Switch } from "react-router";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
-import {history} from "./managers/history";
+import { connect } from "react-redux";
+// import { Login, SignUp } from "./modules";
+import { history } from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
+import BlockChainClass from './modules/explorer'
 
 class Routes extends BaseComponent {
 
@@ -20,9 +21,10 @@ class Routes extends BaseComponent {
             <MuiThemeProvider muiTheme={getMuiTheme()}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path={'/'} component={Login}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
-                        <Redirect exact from='*' to="/"/>
+                        {/* <Route exact path={'/'} component={Login} />
+                        <Route exact path={'/sign-up'} component={SignUp} /> */}
+                        <Route exact path={'/'} component={BlockChainClass} />
+                        <Redirect exact from='*' to="/" />
                     </Switch>
                 </Router>
             </MuiThemeProvider>);
@@ -30,6 +32,6 @@ class Routes extends BaseComponent {
 }
 
 const mapStateToProps = (state) => {
-    return {user: state.user}
+    return { user: state.user }
 };
 export default connect(mapStateToProps)(Routes);
