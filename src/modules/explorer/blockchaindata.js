c3f3108c728b4c8663b74ea29dea051322793ad5
aditya dubey <adityad@leewayhertz.com>
2021-06-15 00:45:45 +0530

    UI for blockchain details on dashboard completed

--- src/modules/explorer/blockchaindata.js
+++ src/modules/explorer/blockchaindata.js
@@ -1,14 +1,25 @@
 import React from "react";
 // import { Row } from 'simple-flexbox'
 import "../../assets/styles/custom.css";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import logo from "../../images/XDC-Icon.svg";
+import blockHeightImg from "../../images/Block Height.svg";
+import priceLogo from "../../images/Gas Price.svg";
+import transactionLogo from "../../images/Transaction.svg";
+import maxLogo from "../../images/Current Max_TPS.svg";
+import difficultyLogo from "../../images/Difficulty.svg";
+import accountLogo from "../../images/Accounts.svg";
+import Tab from "./Tab";
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { BsFillCaretUpFill } from "react-icons/bs";
 
 const MainContainer = styled.div`
-  width: 850px;
-  height: 250px;
+  width: 875px;
+  height: 198px;
   margin: 0 auto;
   margin-top: 76px;
-  padding: 26px 9px 22px 25px;
+  padding-top:20px;
+  padding-left:15px
   border-radius: 12px;
   box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);
   border: solid 1px #e3e7eb;
@@ -16,89 +27,212 @@ const MainContainer = styled.div`
   display: flex;
 `;
 const LeftContainer = styled.div`
-  flex: 0.5;
+  flex: 0.53;
   display: flex;
   flex-direction: column;
 `;
 const RightContainer = styled.div`
-  flex: 0.5;
+  flex: 0.47;
   display: flex;
+  margin-left: 12px;
 `;
 const LeftFirst = styled.div`
   flex: 0.3;
+  position: relative;
   display: flex;
   justify-content: space-between;
-  padding: 0 10px;
   font-size: 2px;
+  align-item: center;
 `;
 const LeftSec = styled.div`
   flex: 0.7;
+  padding-left: 2px;
+  margin-bottom: 2px;
 `;
 const ValueMain = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   flex-wrap: wrap;
-  align-item: center;
+  margin-top: 12px;
 `;
 
 const Value = styled.div`
   display: flex;
-  align-item: center;
+  width: 140px;
+  padding-bottom: 22px;
+`;
+const TitleIcon = styled.img`
+  width: 22%;
+  margin-right: 8px;
 `;
 
-const ValueName = styled.div``;
-
+const ValueName = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-item: flex-end;
+  // margin-left: 2px;
+`;
+const Title = styled.div`
+  color: #a09e9e;
+  font-size: 10px;
+  font-family: "Inter";
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: 0.65px;
+  margin-bottom: 5px;
+`;
+const TitleValue = styled.div`
+  font-size: 13.5px;
+  font-family: Inter;
+  font-weight: bold;
+  line-height: normal;
+  letter-spacing: 0.58px;
+  color: #252525;
+`;
+const LeftTop = styled.div`
+  display: flex;
+  align-item: center;
+  text-align: center;
+  justify-content: center;
+`;
+const IconLogo = styled.img`
+  width:52%;
+  height:52%
+  margin-right: 12px;
+  margin-left:5px;
+`;
+const LeftTitle = styled.div`
+margin-top:3px
+  font-size: 20px;
+  font-weight: 600;
+  font-family: Inter;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: 0.9px;
+  color: #252525;
+`;
+const Line = styled.hr`
+  backgroundcolor: #e3e7eb;
+  width: 440px;
+  position: absolute;
+  top: 55%;
+  left: 1%;
+`;
+const LeftTopSec = styled.div`
+  font-size: 18px;
+  font-weight: 800;
+  margin-right: 3px;
+  font-family: Inter;
+  letter-spacing: 0.55px;
+  color: #252525;
+`;
+const LeftTopSecMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
 export default function BlockChainDataComponent() {
+  let changePrice = -8.156;
+  let changeAccounts = 352;
   return (
     <MainContainer>
       <LeftContainer>
         <LeftFirst>
-          <h1>XDC</h1>
-          <h2>1251</h2>
-        </LeftFirst>
+          <LeftTop>
+            <IconLogo src={logo} />
+            <LeftTitle>XDC</LeftTitle>
+          </LeftTop>
+          <LeftTopSecMain>
+            <LeftTopSec>$0.054024</LeftTopSec>
+            <div
+              className={
+                changePrice > 0 ? "data_value_green" : "data_value_red"
+              }
+            >
+              <div className="value_changePrice">
+                {changePrice > 0 ? (
+                  <div className="arrow_up">
+                    <BsFillCaretUpFill size={10} />
+                  </div>
+                ) : (
+                  <div className="arrow_down">
+                    <BsFillCaretDownFill size={10} />
+                  </div>
+                )}
+                {changePrice}
+              </div>
+            </div>
+          </LeftTopSecMain>
 
+          <Line></Line>
+        </LeftFirst>
         <LeftSec>
           <ValueMain>
             <Value>
-              <h1>1</h1>
+              <TitleIcon src={blockHeightImg} />
               <ValueName>
-                <p style={{ marginBottom: "-3px" }}>Block Height</p>
-                <p>30,080,29</p>
+                <Title>Block Height</Title>
+                <TitleValue>30,080,290</TitleValue>
               </ValueName>
             </Value>
             <Value>
-              <h1>1</h1>
+              <TitleIcon src={priceLogo} />
               <ValueName>
-                <p style={{ marginBottom: "-3px" }}>Block Height</p>
-                <p>30,080,29</p>
+                <Title>Gas Price</Title>
+                <TitleValue>0.0000034</TitleValue>
               </ValueName>
             </Value>
             <Value>
-              <h1>1</h1>
+              <TitleIcon src={transactionLogo} />
               <ValueName>
-                <p style={{ marginBottom: "-3px" }}>Block Height</p>
-                <p>30,080,29</p>
+                <Title>Transactions</Title>
+                <TitleValue>148,875,836</TitleValue>
               </ValueName>
             </Value>
             <Value>
-              <h1>1</h1>
+              <TitleIcon src={difficultyLogo} />
               <ValueName>
-                <p style={{ marginBottom: "-3px" }}>Block Height</p>
-                <p>30,080,29</p>
+                <Title>Difficulty</Title>
+                <TitleValue>85412.0</TitleValue>
               </ValueName>
             </Value>
             <Value>
-              <h1>1</h1>
+              <TitleIcon src={maxLogo} />
               <ValueName>
-                <p style={{ marginBottom: "-3px" }}>Block Height</p>
-                <p>30,080,29</p>
+                <Title>Current/Max TPS</Title>
+                <TitleValue>0/2000</TitleValue>
               </ValueName>
             </Value>
             <Value>
-              <h1>1</h1>
+              <TitleIcon src={accountLogo} />
               <ValueName>
-                <p style={{ marginBottom: "-3px" }}>Block Height</p>
-                <p>30,080,29</p>
+                <Title>Accounts</Title>
+                <div className="last_value">
+                  <TitleValue>24,273</TitleValue>
+                  <div
+                    className={
+                      changeAccounts > 0
+                        ? "data_value_green last_value_main"
+                        : "data_value_red"
+                    }
+                  >
+                    <div className="value_p">
+                      {changeAccounts > 0 ? (
+                        <div className="arrow_up">
+                          <BsFillCaretUpFill size={10} />
+                        </div>
+                      ) : (
+                        <div className="arrow_down">
+                          <BsFillCaretDownFill size={10} />
+                        </div>
+                      )}
+                      {changeAccounts}
+                    </div>
+                  </div>
+                </div>
               </ValueName>
             </Value>
           </ValueMain>
@@ -106,7 +240,7 @@ export default function BlockChainDataComponent() {
       </LeftContainer>
 
       <RightContainer>
-        <h1>right</h1>
+        <Tab />
       </RightContainer>
     </MainContainer>
   );
