090b88fee19ade90f0110a847de66ffb0cd1561a
aditya dubey <adityad@leewayhertz.com>
2021-06-15 11:49:15 +0530

    Latest Blocks and Transactions UI

---
+++ src/modules/explorer/blockData.js
@@ -0,0 +1,64 @@
+const BLOCK_DATA = [
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+    {
+        age: "15 secs ago",
+        height: "30,895,145",
+        trans: "18",
+        reward: "5.5 XDC",
+    },
+
+];
+export default BLOCK_DATA;
