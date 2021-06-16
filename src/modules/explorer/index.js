f03c4294a3afea2a93df37d50d85f9216014a564
sun <xxsunxxrose@gmail.com>
2021-06-16 18:15:11 +0530

    added footer

--- src/modules/explorer/index.js
+++ src/modules/explorer/index.js
@@ -1,11 +1,14 @@
 import BaseComponent from "../baseComponent";
 import React from "react"
 import BlockChainDataComponent from "./blockchaindata";
-
+import FooterComponent from "./footerComponent";
 export default class BlockChainClass extends BaseComponent {
     render() {
         return (
+            <div>
             <BlockChainDataComponent />
+            <FooterComponent />
+            </div>
         )
     }
 }
