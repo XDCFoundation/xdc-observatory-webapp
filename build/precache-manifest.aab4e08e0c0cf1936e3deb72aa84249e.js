042a7e39adc43930e42814881ef7c9f6920efdd9
deepaktomar243 <deepakt@leewayhertz.com>
2021-06-17 21:00:32 +0530

    created build

---
+++ build/precache-manifest.aab4e08e0c0cf1936e3deb72aa84249e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca148493344d212aa31c2475d847441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dca1dcc6f135ce871e2b",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "c41361a5c5cc3eba5696",
+    "url": "/static/css/main.189560a6.chunk.css"
+  },
+  {
+    "revision": "dca1dcc6f135ce871e2b",
+    "url": "/static/js/2.51dcae3e.chunk.js"
+  },
+  {
+    "revision": "c41361a5c5cc3eba5696",
+    "url": "/static/js/main.e5164385.chunk.js"
+  },
+  {
+    "revision": "0e8a657b97d5256e3d96",
+    "url": "/static/js/runtime~main.547ccad9.js"
+  },
+  {
+    "revision": "4cdf742c2a4fdd703e61cc2ab3772b6a",
+    "url": "/static/media/Accounts.4cdf742c.svg"
+  },
+  {
+    "revision": "c639c6a5bbc1289211a0e6361a474f42",
+    "url": "/static/media/Block Height.c639c6a5.svg"
+  },
+  {
+    "revision": "15801967e11dafdd12f44c2a7c5546dc",
+    "url": "/static/media/Current Max_TPS.15801967.svg"
+  },
+  {
+    "revision": "2c7f02986ab162bd9ca69f221d44861c",
+    "url": "/static/media/Difficulty.2c7f0298.svg"
+  },
+  {
+    "revision": "92c0d4da10cd8ead9d538a2de93f8780",
+    "url": "/static/media/Gas Price.92c0d4da.svg"
+  },
+  {
+    "revision": "0697a905edeb094e255bbac3edc8c297",
+    "url": "/static/media/Transaction.0697a905.svg"
+  },
+  {
+    "revision": "5d3f7e751b33bf6cef59781a208c9373",
+    "url": "/static/media/XDC-Icon.5d3f7e75.svg"
+  },
+  {
+    "revision": "645166647ba353ccef5224b0ea34f141",
+    "url": "/static/media/nightmode.64516664.svg"
+  }
+]);
