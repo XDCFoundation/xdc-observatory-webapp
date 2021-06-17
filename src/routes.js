3fa773aa9e3d2d8f13103befabed2a44443b8b23
deepaktomar243 <deepakt@leewayhertz.com>
2021-06-17 21:00:32 +0530

    created build

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { Redirect, Switch } from "react-router";
+import {Router, Route} from 'react-router-dom';
+import {Redirect, Switch} from "react-router";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { connect } from "react-redux";
-import { history } from "./managers/history";
+import {connect} from "react-redux";
+import {history} from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
 
 import BlockChainClass from './modules/explorer/';
@@ -20,10 +20,9 @@ class Routes extends BaseComponent {
 
             <MuiThemeProvider muiTheme={getMuiTheme()}>
                 <Router history={history}>
-
                     <Switch>
-                        <Route exact path={'/'} component={BlockChainClass} />
-                        <Redirect exact from='*' to="/" />
+                        <Route exact path={'/'} component={BlockChainClass}/>
+                        <Redirect exact from='*' to="/"/>
                     </Switch>
                 </Router>
             </MuiThemeProvider>
@@ -32,6 +31,6 @@ class Routes extends BaseComponent {
 }
 
 const mapStateToProps = (state) => {
-    return { user: state.user }
+    return {user: state.user}
 };
 export default connect(mapStateToProps)(Routes);
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 /**
- * Created by Ayush Kulshrestha on 18/09/2019.
+ * Created by Developer on 18/09/2019.
  */
 //export all services from index file -
 
