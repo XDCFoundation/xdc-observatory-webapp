5b748ef2d7fdf5726bddc86d066f1c2b368d34b2
deepaktomar243 <deepakt@leewayhertz.com>
2021-06-30 20:03:21 +0530

    changed package.json

--- src/modules/explorer/blockDetails.js
+++ src/modules/explorer/blockDetails.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
