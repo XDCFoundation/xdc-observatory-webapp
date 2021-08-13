e5d08c2dbc08ffd9515e364a2bdcf2ac74243753
Alok Saxena <alok@leewayhertz.com>
2021-08-13 11:42:40 +0530

    token market cap integration

--- src/services/token.js
+++ src/services/token.js
@@ -1,7 +1,23 @@
 import { httpService } from "../managers/httpService";
 import { httpConstants } from "../images/constants";
 
-export default { getTokenLists, getTotalToken, getTokenSearch, getTotalTransferTransactionsForToken }
+export default { CoinMarketExchangeForToken , getTokenLists, getTotalToken, getTokenSearch, getTotalTransferTransactionsForToken }
+
+async function CoinMarketExchangeForToken(data) {
+    let url = process.env.REACT_APP_GET_TOKEN_MARKETCAP + '/'+data;
+
+    return httpService(httpConstants.METHOD_TYPE.GET, { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, {}, url)
+        .then(
+            response => {
+                if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0) {
+                    return Promise.reject();
+                }
+                return Promise.resolve(response.responseData);
+            }
+        ).catch(function (err) {
+            return Promise.reject(err);
+        });
+}
 
 async function getTokenLists(data) {
     let url = process.env.REACT_APP_GET_TOKEN_LIST + '?skip=' + Math.ceil(data.pageNum) + '&limit=' + data.perpage;
