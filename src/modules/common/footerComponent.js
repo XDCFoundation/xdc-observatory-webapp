3dfd5a85c80bf77a730a1686a75f4080e9110f71
aditya dubey <adityad@leewayhertz.com>
2021-09-23 22:46:06 +0530

    DashBoard Fixed in Rem

--- src/modules/common/footerComponent.js
+++ src/modules/common/footerComponent.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "../../assets/styles/footer.css";
 import "../../assets/styles/custom.css";
-import INR from "../../assets/images/inr.png";
-import EUR from "../../assets/images/euro.png";
-import USD from "../../assets/images/usd.png";
+import INR from "../../assets/images/INR.svg";
+import EUR from "../../assets/images/Euro.svg";
+import USD from "../../assets/images/dollar.svg";
 import Grid from "@material-ui/core/Grid";
 import Select from "@material-ui/core/Select";
 import { MenuItem } from "material-ui";
