a58fde149e9120135d534791bb1175c5b3273782
tushar <tushar@leewayhertz.com>
2021-09-27 18:28:46 +0530

    copy icon fixed

--- src/modules/address/addressDetails.js
+++ src/modules/address/addressDetails.js
@@ -186,7 +186,7 @@ export default function AddressDetails(props) {
                         placement="top"
                       >
                         <button style={{ color: 'blue', backgroundColor: 'white', fontSize: 17}}>
-                          <i class="fa fa-clone" aria-hidden="true"></i></button>
+                        <img src={require("../../../src/assets/images/copy.svg")} /></button>
                       </Tooltip>
                     </CopyToClipboard>
                     <Popup trigger={<ImQrcode style={{ marginLeft: "0.625rem", marginBottom: "0.125rem", cursor: "pointer", color: "#2149b9", height: "1.063rem", width: "1.063rem"}} />} modal>
