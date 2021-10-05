29913bd88fcc9ffd3b44edcf21fef46476fd27b6
kumarisakshi <kumarisakshi2723@gmail.com>
2021-10-05 18:30:45 +0530

    done

--- src/modules/explorer/editprofle.js
+++ src/modules/explorer/editprofle.js
@@ -1,16 +1,36 @@
-import React from "react";
-
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
+import React, { useMemo, useEffect, useState, useRef } from "react";
 
-import { makeStyles, mergeClasses } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/styles";
+
+import { useDropzone } from "react-dropzone";
 
 import { history } from "../../managers/history";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: "auto",
+  height: 200,
+  padding: 4,
+  boxSizing: "border-box",
+};
 
 const useStyles = makeStyles((theme) => ({
   add: {
@@ -19,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
     // fontFamily: "Roboto",
     // fontStyle: "normal",
     backgroundColor: "#2149b9",
-    marginLeft: "90px"
+    marginLeft: "90px",
   },
   btn: {
     // border: "none !important",
@@ -37,14 +57,14 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "25px",
     marginLeft: "40px",
     fontWeight: "500",
-    cursor: "pointer"
+    cursor: "pointer",
   },
   dialog: {
     marginLeft: "10%",
     marginTop: "2px",
     width: "80% !important",
     height: "80% !important",
-    borderRadius: "50px !important"
+    borderRadius: "50px !important",
   },
   input: {
     width: "340px",
@@ -71,11 +91,11 @@ const useStyles = makeStyles((theme) => ({
     fontsize: "14px",
     fontweight: "500",
     border: "none !important",
-    outline: "none"
+    outline: "none",
   },
   forgotpass: {
     color: "#2149b9",
-    marginLeft: "123px"
+    marginLeft: "123px",
   },
   createaccount: {
     color: "#2149b9",
@@ -84,7 +104,7 @@ const useStyles = makeStyles((theme) => ({
     fontsize: "14px",
   },
   icon: {
-    marginLeft: "-30px"
+    marginLeft: "-30px",
   },
   xdc: {
     color: "#2a2a2a",
@@ -95,15 +115,30 @@ const useStyles = makeStyles((theme) => ({
   heading: {
     marginLeft: "60px",
     fontfamily: "Inter",
-    fontweight: "600"
-  }
+    fontweight: "600",
+  },
 }));
-const Wrapper=styled.div`
-display:flex;
-flex-direction:row;
-justify-content:space-between;
-padding-top:20px;`;
-const Upload=styled.div`
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+const baseStyle = {
+  borderRadius: 5,
+
+  height: "104px",
+  width: "400px",
+  background: "#FFFFFF 0% 0% no-repeat padding-box",
+  color: "#ACACAC",
+  border: "0.3px dashed var(--unnamed-color-acacac)",
+  border: "0.30000001192092896px dashed #ACACAC",
+  // marginTop: 5,
+};
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 20px;
+`;
+const Upload = styled.div`
 
 display: block;
 margin-left: auto;
@@ -119,28 +154,25 @@ font-family: Inter;
 }
 HTML
 `;
-
-
-const Title=styled.div`
-font-family: Inter;
-font-size: 22px;
-font-weight: 600;
-text-align: center;
-  color: #2a2a2a;
-  padding-left:32px;
-
- 
-  
- 
+const Image = styled.img`
+  width: 100%;
+  height: 145px;
+  border-radius: 4px;
+  &:focus {
+    outline: none;
+  }
 `;
 
-  
- 
-  
- 
+const Title = styled.div`
+  font-family: Inter;
+  font-size: 22px;
+  font-weight: 600;
+  text-align: center;
+  color: #2a2a2a;
+  padding-left: 32px;
+`;
 
-
-const Cut =styled.div`
+const Cut = styled.div`
 
 padding-right: 25px;
     padding-top: 7px;
@@ -148,7 +180,7 @@ padding-right: 25px;
     display:flex;
     align-content: flex-end;
 }`;
-const Input =styled.div`
+const Input = styled.div`
 display:flex;
 flex-flow: row nowrap;
 
@@ -156,16 +188,8 @@ flex-flow: row nowrap;
 }`;
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
-
-  const [passwordShown, setPasswordShown] = React.useState(false);
-  const togglePasswordVisiblity = () => {
-    setPasswordShown(passwordShown ? false : true);
-    // {passwordShown ?<VisibilityIcon/>:<VisibilityOff/>}
-
-  };
-
   const classes = useStyles();
+  const [opens, setOpen] = useState(true);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -175,96 +199,180 @@ export default function FormDialog() {
     setOpen(false);
   };
 
-
   const handleLogin = () => {
-    history.push("/loginprofile")
+    history.push("/loginprofile");
     // window.location("/loginprofile")
+  };
 
-  }
+  const [files, setFiles] = useState([]);
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+    acceptedFiles,
+    open,
+  } = useDropzone({
+    accept: "image/*",
+    noClick: true,
+    noKeyboard: true,
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const handleUpload = (event) => {
+    setFiles(event.target.files[0]);
+  };
+
+  const ImageThumb = ({ image }) => {
+    return <Image src={URL.createObjectURL(image)} alt={image.name} />;
+  };
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject]
+  );
+
+  const thumbs = files.map((file) => <div style={thumb} key={file.name}></div>);
+
+  useEffect(
+    () => () => {
+      console.log("file",files);
+      console.log("accept",acceptedFiles);
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
+
+  const filepath = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const [passwordShown, setPasswordShown] = React.useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+    // {passwordShown ?<VisibilityIcon/>:<VisibilityOff/>}
+  };
 
   return (
     <div>
-
       <div className={classes.add}>
-        <button
-          className="login-button"
-
-          onClick={handleClickOpen}
-        >
-           <div className="edit">
-                        Edit Profile
-                    </div>
-         
+        <button className="login-button" onClick={handleClickOpen}>
+          <div className="edit">Edit Profile</div>
         </button>
 
         <div>
           <Dialog
             className={classes.dialog}
-            open={open}
+            open={opens}
             onClose={handleClose}
             aria-labelledby="form-dialog-title"
           >
-           <Wrapper>
+            <Wrapper>
               <div></div>
               <Title>Edit Profile</Title>
+
+              <Cut onClick={handleClose}> X </Cut>
+            </Wrapper>
+           
+            <div className="">
+              {filepath.length === 0 ? (
+                <div className="upload-box">
+                   <img
+              className="dialogeUser center"
+              src={require("../../../src/assets/images/Profile.svg")}
+            />
+                  <input {...getInputProps()} />
+
+                  
+                  
+                   
+                    <span
+                      style={{
+                        color: "#7D84C0",
+                        cursor: "pointer",
+                      }}
+                      onClick={open}
+                    >
+                      browse
+                    </span>
+                 
+                </div>
+              ) : (
+                
+                 
+                  <div>
+                   
+                  <img src={files} alt="image is not showing" />
+                   <p className="dragcont" style={{ textAlign: "center" }}>{filepath}</p> 
+                   </div>
               
-              <Cut onClick={handleClose} >
-                {" "}
-                X{" "}
-              </Cut>
-              </Wrapper>
-              
-              <img className="dialogeUser center"src={require("../../../src/assets/images/Profile.svg")}/>
-             {/* <Upload input type="file">Upload Photo</Upload> */}
-             <span
-              style={{
-                color: "#2149b9",
-                cursor: "pointer",
-                display: "block",
-                marginLeft: "auto",
-                marginRight: "auto",
-              }}
-              onClick={open}
-            >
-          Upload Photo
-            </span>
-             {/* <input type="file" id="myFile" name="filename"> */}
+              )}
+            </div>
             <DialogContent>
               <DialogContentText className={classes.subCategory}>
                 <b>Username</b>
               </DialogContentText>
               <Input className="inputcss">
-              <input style={{  backgroundColor: "#f5f5f5"}} className="hide-border"type="text"/ >
-              <img className="imgcss"src={require("../../../src/assets/images/edit.svg")}/> 
+                <input
+                  style={{ backgroundColor: "#f5f5f5" }}
+                  className="hide-border"
+                  type="text"
+                />
+                <img
+                  className="imgcss"
+                  src={require("../../../src/assets/images/edit.svg")}
+                />
               </Input>
             </DialogContent>
             <DialogContent>
               <DialogContentText className={classes.subCategory}>
                 <b>Email</b>
-              
-               
               </DialogContentText>
 
-            
               <Input className="inputcss">
-              <input style={{  backgroundColor: "#f5f5f5"}}className="hide-border"type="text"/ >
-              <img className="imgcss"src={require("../../../src/assets/images/edit.svg")}/> 
-              </Input>          
-            
+                <input
+                  style={{ backgroundColor: "#f5f5f5" }}
+                  className="hide-border"
+                  type="text"
+                />
+                <img
+                  className="imgcss"
+                  src={require("../../../src/assets/images/edit.svg")}
+                />
+              </Input>
             </DialogContent>
             <DialogActions>
-              <button className={classes.addbtn} onClick={handleLogin}
-                onClick={event => window.location.href = 'loginprofile'}
-
-              >Update Profile </button>
+              <button
+                className={classes.addbtn}
+                onClick={handleLogin}
+                onClick={(event) => (window.location.href = "loginprofile")}
+              >
+                Update Profile{" "}
+              </button>
               {/* <Link to="/loginprofile" className={classes.addbtn} className="btn btn-primary">Log in</Link> */}
             </DialogActions>
             <div className={classes.value}></div>
-           
           </Dialog>
         </div>
       </div>
-
     </div>
   );
 }
