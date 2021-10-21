380caa2a68cab6b98f0e1c1c94e00b8003ca341b
tushar <tushar@leewayhertz.com>
2021-10-21 19:36:45 +0530

    integrate get getuserdata

---
+++ src/services/user.js
@@ -0,0 +1,40 @@
+import { httpService } from "../managers/httpService";
+import { httpConstants } from "../images/constants";
+
+export default { getUserPrivateNote , postUserPrivateNote}
+async function getUserPrivateNote(data) {
+
+    
+    let url = `http://xinfin-explorer-elb-944849870.us-east-1.elb.amazonaws.com:3002/transaction-private-note/${data}`;
+    // console.log("url ",url)
+    return httpService("GET", { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON },{}, url)
+        .then(
+            response => {
+                if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0)
+                    return Promise.reject();
+                return Promise.resolve(response.responseData);
+
+            }
+        ).catch(function (err) {
+            return Promise.reject(err);
+        });
+}
+
+async function postUserPrivateNote(data) {
+    
+
+    
+    let url = "http://xinfin-explorer-elb-944849870.us-east-1.elb.amazonaws.com:3002/add-transaction-label";
+    console.log("url ",url)
+    return httpService("POST", { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, data, url)
+        .then(
+            response => {
+                if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0)
+                    return Promise.reject();
+                return Promise.resolve(response.responseData);
+
+            }
+        ).catch(function (err) {
+            return Promise.reject(err);
+        });
+}
