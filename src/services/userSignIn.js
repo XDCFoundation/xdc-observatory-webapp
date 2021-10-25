b2fdca1561e5ed8eb0a7e0a4cd6ba0a69eafa7e8
Faraz_FarazAhmad <faraz@leewayhertz.com>
2021-10-25 22:51:36 +0530

    api updated

---
+++ src/services/userSignIn.js
@@ -0,0 +1,23 @@
+import { httpService } from "../managers/httpService";
+import { httpConstants } from "../images/constants";
+
+export default { postSignIn}
+
+async function postSignIn(data) {
+    
+
+    
+    let url = "http://xinfin-explorer-elb-944849870.us-east-1.elb.amazonaws.com:3003/login";
+    console.log("url ",url)
+    return httpService("POST", { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, data, url)
+        .then(
+            response => {
+                if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0)
+                    return Promise.reject();
+                return Promise.resolve(response.responseData);
+
+            }
+        ).catch(function (err) {
+            return Promise.reject(err);
+        });
+}
