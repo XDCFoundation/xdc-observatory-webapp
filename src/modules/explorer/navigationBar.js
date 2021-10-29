3953faf3bf8dd81228f86d84167a2db15c123a1f
Dhanya_DhanyaMary <dhanya@leewayhertz.com>
2021-10-29 15:48:07 +0530

    display message toast

--- src/modules/explorer/navigationBar.js
+++ src/modules/explorer/navigationBar.js
@@ -164,6 +164,10 @@ export default function Navbar() {
       const [error, responseData] = await Utility.parseResponse(
         SearchData.searchData(data)
       );
+      if (!responseData)
+      {
+        Utility.apiFailureToast("No details found.");
+      }
 
       if (responseData) {
         console.log(responseData, "KIu")
