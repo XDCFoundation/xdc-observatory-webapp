3c1e54a58225b71aed505a96e481530280275f27
Faraz_FarazAhmad <faraz@leewayhertz.com>
2021-11-11 19:13:59 +0530

    api integration

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,7 +1,7 @@
 import { httpService } from "../managers/httpService";
 import { httpConstants } from "../constants";
 
-export default { getTotalTransaction, getLatestTransaction, getSomeDaysTransaction, getTransactionDetailsUsingHash }
+export default { getTotalTransaction, getLatestTransaction, getSomeDaysTransaction, getTransactionDetailsUsingHash, getUserTransactionPrivateNoteUsingHash, getUserAddressTagUsingAddressHash }
 async function getTotalTransaction() {
     let url = process.env.REACT_APP_GET_TOTAL_TRANSACTION;
     return httpService(httpConstants.METHOD_TYPE.GET, { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, {}, url)
@@ -49,6 +49,36 @@ async function getSomeDaysTransaction() {
 async function getTransactionDetailsUsingHash(path, data) {
     let url = process.env.REACT_APP_GET_TRANSACTION_DETAILS + path;
     return httpService(httpConstants.METHOD_TYPE.GET, { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, data, url)
+        .then(
+            response => {
+                if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0)
+                    return Promise.reject();
+                return Promise.resolve(response.responseData);
+
+            }
+        ).catch(function (err) {
+            return Promise.reject(err);
+        });
+}
+
+async function getUserTransactionPrivateNoteUsingHash(data) {
+    let url = process.env.REACT_APP_GET_USER_TRANSACTION_PRIVATE_NOTE_USING_HASH ;
+    return httpService(httpConstants.METHOD_TYPE.POST, { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, data, url)
+        .then(
+            response => {
+                if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0)
+                    return Promise.reject();
+                return Promise.resolve(response.responseData);
+
+            }
+        ).catch(function (err) {
+            return Promise.reject(err);
+        });
+}
+
+async function getUserAddressTagUsingAddressHash(data) {
+    let url = process.env.REACT_APP_GET_USER_ADDRESS_TAG_USING_ADDRESS_HASH ;
+    return httpService(httpConstants.METHOD_TYPE.POST, { 'Content-Type': httpConstants.CONTENT_TYPE.APPLICATION_JSON }, data, url)
         .then(
             response => {
                 if (!response.success || response.responseCode !== 200 || !response.responseData || response.responseData.length === 0)
