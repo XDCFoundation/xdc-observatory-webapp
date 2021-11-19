20b4decb88780a6f20851abb76284ed143165d1b
aditya dubey <adityad@leewayhertz.com>
2021-11-19 12:17:37 +0530

    build-1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4276a7a8eed7b03f683ebe06a82ecb17.js"
+  "/precache-manifest.2d2e8b56a172d1ecf65e958b2493eea6.js"
 );
 
 self.addEventListener('message', (event) => {
