eff350715ccf91e02bb40289e1379fb9ad09e67e
Anmol Tiwari <anmol@leewayhertz.com>
2021-12-11 19:41:31 +0530

    fixed

--- src/modules/explorer/loginDialog.js
+++ src/modules/explorer/loginDialog.js
@@ -262,7 +262,7 @@ const useStyles = makeStyles((theme) => ({
     paperWidthSm: {
       position: "absolute",
       // top: "102px",
-      maxWidth: "503px",
+      maxWidth: "767px",
       width: "100%",
       height: "100%",
       borderRadius: "0px",
