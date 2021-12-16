0dba450ca40191f0aff49ea87b4b1618155945ba
aditya dubey <adityad@leewayhertz.com>
2021-12-16 12:05:14 +0530

    Merged XIN-AD-REM

--- src/modules/explorer/tokensearchBar.js
+++ src/modules/explorer/tokensearchBar.js
@@ -446,13 +446,7 @@ export default function Navbar() {
                 <i class="fa fa-angle-left" aria-hidden="true"></i>
               </span>{" "}
             </div>
-            <Cut onClick={handleClose}>
-              {" "}
-              <img
-                className="cross-icon"
-                src={require("../../../src/assets/images/back.svg")}
-              />{" "}
-            </Cut>
+
             <div
               style={{
                 color: "white",
