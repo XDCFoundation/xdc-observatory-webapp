7b8e1adfccec504896d0b0f117cf64cf835603f3
Deepak Tomar <deepakt@leewayhertz.com>
2021-12-20 21:02:59 +0530

    build prod

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.472cf00506d3aeb3206c297eeb4a04dc.js"
+  "/precache-manifest.a85c72d7f9eef32977bdc0908626fd67.js"
 );
 
 self.addEventListener('message', (event) => {
