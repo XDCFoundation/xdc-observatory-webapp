02044b5713e14b74c36ecd535d1464cdc7583815
Rachit_RachitMishra <rachit@leewayhertz.com>
2021-12-23 12:55:13 +0530

    bugs fixed

--- src/modules/explorer/navigationBar.js
+++ src/modules/explorer/navigationBar.js
@@ -705,7 +705,7 @@ export default function Navbar() {
 
                 {/* <a href="/">
                   <p className="Network-explorer" id="Network-explorer">
-                    XDC Observatory
+                    Network
                   </p>
                 </a>
               </div>
@@ -758,60 +758,17 @@ export default function Navbar() {
         <AppBar elevation={0} className={clsx(classes.appBar)}>
           <MobileToolBar>
             <Typography className="Header">
-              <div className="header-mobile-top">
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <a className="logo_tokensearch" href={"/"}>
-                    <img
-                      className="Shape"
-                      src={"/images/XDC icon.svg"}
-                    ></img>
-                  </a>
-                  <a className="XDC" href="/">
-                    {" "}
-                    XDC{" "}
-                  </a>
-                </div>
-                &nbsp;
-                <div className="header-responsive">
-                  <div>
-                    <NavLink
-                      exact
-                      activeClassName="active-t"
-                      to={"/"}
-                      className="Network-explorer"
-                    >
-                      XDC Observatory
-                    </NavLink>
-                  </div>
-
-                  <div>
-                    <a href="/token-details">
-                      <div className="Token" id="Token">
-                        Tokens
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="header-responsive-desktop">
-                <div>
-                  <NavLink
-                    exact
-                    activeClassName="active-t"
-                    to={"/"}
-                    className="Network-explorer"
-                  >
-                    XDC Observatory
-                  </NavLink>
-                </div>
-
-                <div>
-                  <a href="/token-details">
-                    <div className="Token" id="Token">
-                      Tokens
-                    </div>
-                  </a>
-                </div>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <a className="logo_tokensearch" href={"/"}>
+                  <img
+                    className="Shape"
+                    src={"/images/XDC-Icon-Logo.svg"}
+                  ></img>
+                </a>
+                <a className="XDC" href="/">
+                  {" "}
+                  XDC{" "}
+                </a>
               </div>
             </Typography>
             <Row alignItems="center">
@@ -857,10 +814,7 @@ export default function Navbar() {
       </MobileView>
       <main className={clsx(classes.content)}>
         <div className="exp-parent">
-          <img
-            className="Shape3"
-            src={"images/Networkexplorer.svg"}
-          ></img>
+          <img className="Shape3" src={"/images/Networkexplorer.svg"}></img>
           <div className="exp">XDC Observatory</div>
         </div>
         {/* ------------ Search bar ----------------- */}
