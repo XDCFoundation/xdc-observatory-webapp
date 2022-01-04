74345be5de4459dd7cc2664c3a8ecde888ff6233
anuragkumar <anurag@leewayhertz.com>
2022-01-04 15:48:09 +0530

    build

--- src/modules/token/analytics/tokenBalanceGraph/tokenBalanceGraphComponent.js
+++ src/modules/token/analytics/tokenBalanceGraph/tokenBalanceGraphComponent.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Graph from "../../../common/commonGraph"
 
+const options = {
+    title: {
+        text: "My stock chart",
+    },
+    series: [
+        {
+            data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9, 15],
+        },
+    ],
+};
 
 function TokenBalanceGraphComponent(props) {
     return (
         <div>
-
+            <Graph options={options}/>
         </div>
     )
 }
