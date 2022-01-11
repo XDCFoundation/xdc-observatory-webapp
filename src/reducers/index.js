146961c03d98eda69d0189d62a20f621ccb936eb
sunnyk56 <sunnyk@leewayhertz.com>
2022-01-11 11:19:29 +0530

    timezone ui

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import activeCurrency from "./currency"
+import timezone from "./timezone";
 
 export default combineReducers({
     user,
-    activeCurrency
+    activeCurrency,
+    timezone
 });
