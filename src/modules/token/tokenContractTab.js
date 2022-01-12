824def5f4f6724246e57a077fb2e59897b31479a
Praveen_PraveenKumar <praveenk@leewayhertz.com>
2022-01-12 14:42:06 +0530

    env file updated

--- src/modules/token/tokenContractTab.js
+++ src/modules/token/tokenContractTab.js
@@ -31,7 +31,7 @@ const TabButtonContainer = styled.div`
 `;
 
 export default function TokenContracttab(props) {
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(2);
 
   return (
     <>
