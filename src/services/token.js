9ac3ba0a74c363a8806714e9d13114f359ecb004
ayushkul <akcool.ayush.svm@gmail.com>
2022-01-13 19:57:12 +0530

    Token Search and Filters

--- src/services/token.js
+++ src/services/token.js
@@ -135,7 +135,8 @@ async function getListOfTransferTransactionsForToken(data) {
     "?skip=" +
     Math.ceil(data.pageNum) +
     "&limit=" +
-      data.perpage + (data.searchValue ? "&searchValue=" + data.searchValue : "");
+      data.perpage + (data.searchValue ? "&searchValue=" + data.searchValue : "")
+      + (data.startDate ? "&startDate=" + data.startDate : "");
 
   return httpService(httpConstants.METHOD_TYPE.GET, getHeaders(), {}, url)
     .then((response) => {
@@ -153,7 +154,8 @@ async function getListOfTransferTransactionsForToken(data) {
     });
 }
 async function getTotalTransferTransactionsForToken(data) {
-    let url = process.env.REACT_APP_GET_TOTAL_TRANSFER_FOR_TOKEN + data.addr + (data.searchValue ? "?searchValue=" + data.searchValue : "");
+    let url = process.env.REACT_APP_GET_TOTAL_TRANSFER_FOR_TOKEN + data.addr + (data.searchValue ? "?searchValue=" + data.searchValue : "")
+        + (data.startDate ? (data.searchValue ? "&startDate=" : "?startDate=") + data.startDate : "");
 
   return httpService(httpConstants.METHOD_TYPE.GET, getHeaders(), {}, url)
     .then((response) => {
