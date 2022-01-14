0f41d1f9ba96521dd1fac06136a05d01ba2e40bc
Aamir Khan <aamir@leewayhertz.com>
2022-01-14 21:08:03 +0530

    changes

--- src/modules/explorer/accountProfile.js
+++ src/modules/explorer/accountProfile.js
@@ -378,9 +378,9 @@ const UserNameContainer = styled.div`
 
 
   @media (max-width: 850px) {
-    padding: 0 10px 0 10px !important;
+    ${'' /* padding: 0 10px 0 10px !important;
     flex-wrap : nowrap;
-    max-width: 710px;
+    max-width: 710px; */}
   }
   @media (min-width: 400px) and (max-width: 767px){
     ${'' /* gap: 12px; */}
