acbe93073b871f91b51265c64bdaaa44a25771da
anuragkumar <anurag@leewayhertz.com>
2022-01-28 18:35:22 +0530

    sorting implemented in transfer table of token details page

--- src/services/watchlist.js
+++ src/services/watchlist.js
@@ -1,69 +1,67 @@
-import { httpService } from "../managers/httpService";
-import { cookiesConstants, httpConstants } from "../constants";
+import {httpService} from "../managers/httpService";
+import {cookiesConstants, httpConstants} from "../constants";
+
 const isLocalStorage = true;
 
-export default { postWatchlist, deleteWatchlist };
+export default {postWatchlist, deleteWatchlist};
+
 function getHeaders() {
-  return {
-    "Content-Type": httpConstants.CONTENT_TYPE.APPLICATION_JSON,
-    "X-API-key": process.env.REACT_APP_X_API_KEY,
-  };
+    return {
+        "Content-Type": httpConstants.CONTENT_TYPE.APPLICATION_JSON,
+        "X-API-key": process.env.REACT_APP_X_API_KEY,
+    };
 }
+
 async function postWatchlist(reqObj) {
-  const url = process.env.REACT_APP_POST_WATCHLIST + "";
-  return httpService(httpConstants.METHOD_TYPE.POST, getHeaders(), reqObj, url)
-    .then((response) => {
-      if (
-        !response.success ||
-        response.responseCode !== 200 ||
-        !response.responseData ||
-        response.responseData.length === 0
-      )
-        return Promise.reject();
-      return Promise.resolve(response.responseData);
-    })
-    .catch(function (err) {
-      return Promise.reject(err);
-    });
+    const url = process.env.REACT_APP_POST_WATCHLIST + "";
+    return httpService(httpConstants.METHOD_TYPE.POST, getHeaders(), reqObj, url)
+        .then((response) => {
+            if (
+                !response.success ||
+                response.responseCode !== 200 ||
+                !response.responseData ||
+                response.responseData.length === 0
+            )
+                return Promise.reject();
+            return Promise.resolve(response.responseData);
+        })
+        .catch(function (err) {
+            return Promise.reject(err);
+        });
 }
 
 async function deleteWatchlist(reqObj, req) {
-  if (isLocalStorage && req && req.userId && req.address) {
+    if (!isLocalStorage || !req || !req.userId || !req.address) {
+        return []
+    }
     let addressTags = localStorage.getItem(
-      cookiesConstants.USER_ADDRESS_WATCHLIST
+        req.userId + cookiesConstants.USER_ADDRESS_WATCHLIST
     );
     if (addressTags) {
-      addressTags = JSON.parse(addressTags);
-      let index = addressTags.findIndex((item) => {
-        if (item.userId == req.userId && item.address == req.address) {
-          return item;
+        addressTags = JSON.parse(addressTags);
+        if (addressTags[req.address]) {
+            delete addressTags[req.address];
+            localStorage.setItem(
+                req.userId + cookiesConstants.USER_ADDRESS_WATCHLIST,
+                JSON.stringify(addressTags)
+            );
         }
-      });
-      if (index !== -1) {
-        addressTags.splice(index, 1);
-        localStorage.setItem(
-          cookiesConstants.USER_ADDRESS_WATCHLIST,
-          JSON.stringify(addressTags)
-        );
-      }
     }
-    return [];
-  }
 
-  const url =
-    process.env.REACT_APP_WATCHLIST_TRANSACTION_SERVICE + "delete-watchlist";
-  return httpService(httpConstants.METHOD_TYPE.PUT, getHeaders(), reqObj, url)
-    .then((response) => {
-      if (
-        !response.success ||
-        response.responseCode !== 200 ||
-        !response.responseData ||
-        response.responseData.length === 0
-      )
-        return Promise.reject();
-      return Promise.resolve(response.responseData);
-    })
-    .catch(function (err) {
-      return Promise.reject(err);
-    });
+    const url =
+        process.env.REACT_APP_WATCHLIST_TRANSACTION_SERVICE + "delete-watchlist";
+    return httpService(httpConstants.METHOD_TYPE.PUT, getHeaders(), reqObj, url)
+        .then((response) => {
+            if (
+                !response.success ||
+                response.responseCode !== 200 ||
+                !response.responseData ||
+                response.responseData.length === 0
+            )
+                return Promise.reject();
+            return Promise.resolve(response.responseData);
+        })
+        .catch(function (err) {
+            return Promise.reject(err);
+        });
 }
