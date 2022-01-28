8cd390e6da182ff387c8e7f04c1e525b23b6f34c
kajal_KajalBobal <kajal@leewayhertz.com>
2022-01-28 19:50:08 +0530

    login sdk integerated

--- src/services/auth0.js
+++ src/services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from "auth0-js";
+import { cookiesConstants } from "../constants";
 import { sessionManager } from "../managers/sessionManager"
 
 export default class Auth0Service {
@@ -6,26 +7,38 @@ export default class Auth0Service {
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-      scope: 'openid profile email',
-      responseType: 'code token id_token',
-      grantType: 'password',
-      redirectUri: window.location.origin
+      audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+      redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
+      scope: process.env.REACT_APP_AUTH0_SCOPE,
+      responseType: process.env.REACT_APP_AUTH0_RESPONSE_TYPE,
+      grantType:  process.env.REACT_APP_AUTH0_GRANT_TYPE
     });
+    // this.signin = this.signin.bind(this);
   }
-
-  signin(email, password) {
+  signin(username, password) {
     let _this = this;
     return new Promise((resolve, reject) => {
       this.auth0.client.login(
-        { realm: process.env.REACT_APP_AUTH0_REALM , username: email, password},
+        { realm: process.env.REACT_APP_AUTH0_REALM,username:username, password:password},
         async (err, authResult) =>{
           if(err) {
             return reject(err);
           }
-          sessionManager.setDataInCookies(authResult.accessToken)
-          sessionManager.setDataInCookies(authResult.idToken)
-          let userDetail = await _this.getUserInfo(authResult.idToken)
-          resolve(userDetail)
+          sessionManager.setDataInCookies(authResult.accessToken,cookiesConstants.ACCESS_TOKEN )
+          sessionManager.setDataInCookies(authResult.idToken, cookiesConstants.AUTH0_ID_TOKEN)
+          _this.auth0.client.userInfo(
+            authResult.accessToken,
+            async (err, user) => {
+              if (err) {
+                return reject(err);
+              }
+              return resolve({
+                ...authResult,
+                userInfoRes: user,
+                userMetaData: user,
+              });
+            }
+          );
         }
       )
     })
