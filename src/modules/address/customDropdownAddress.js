c5bf8daf40d09bfcf8785db6721d8cb3ea22abf2
Faraz_FarazAhmad <faraz@leewayhertz.com>
2022-03-30 18:33:47 +0530

    updated-ui

--- src/modules/address/customDropdownAddress.js
+++ src/modules/address/customDropdownAddress.js
@@ -12,6 +12,7 @@ const Container = styled.div`
     margin-left: 0;
     margin-top: 4px;
     margin-bottom: 4px;
+    width: 180px;
   }
 `;
 const SelectedValueContainer = styled.div`
@@ -29,6 +30,10 @@ const SelectedValueContainer = styled.div`
     width: 11px;
     margin-left: 8px;
   }
+  ${({ theme }) => theme === "dark" && `
+    background: transparent;
+    border: solid 1px #3552a5;
+  `}
 `;
 
 const FilterName = styled.div`
@@ -61,9 +66,18 @@ const DropdownContainer = styled.div`
   min-width: ${(props) => props.containerWidth}px;
   max-height: 347px;
   padding-bottom: 10px;
+  ${({ theme }) => theme === "dark" && `
+    background: #283966;
+    border: solid 1px #3552a5;
+    color: #ffffff;
+  `}
   span {
     padding: 8px;
     cursor: pointer;
+  }
+  @media (max-width: 767px) {
+    max-width: 180px;
+    padding-right: 5px;
   }
 `;
 const SearchBox = styled.div`
@@ -76,9 +90,16 @@ const SearchBox = styled.div`
   display: flex;
   align-items: center;
   margin: 6px 3px;
+  ${({ theme }) => theme === "dark" && `
+    background: #091b4e;
+    border: solid 1px #3552a5;
+  `}
   input {
     outline: none;
     border: none !important;
+    ${({ theme }) => theme === "dark" && `
+    background: #091b4e;
+  `}
   }
 
   img {
@@ -90,6 +111,11 @@ const SearchBox = styled.div`
   @media (max-width: 767px) {
     min-width: 100%;
     margin-right: 0;
+    input {
+      outline: none;
+      border: none !important;
+      width: 136px;
+    }
   }
 `;
 const TokenBalanceContainer = styled.div`
@@ -104,6 +130,9 @@ const OptionDiv = styled.div`
   :hover {
     background-color: #3763dd;
     color: #fff !important;
+  }
+  @media (max-width: 767px) {
+    display: block;
   }
 `;
 const TokenName = styled.div`
@@ -122,11 +151,17 @@ const TokenHeading = styled.div`
   display: flex;
   align-items: center;
   padding-left: 4px;
-
+  ${({ theme }) => theme === "dark" && `
+    background: #091b4e;
+    color: #ffffff;
+  `}
   span {
     font-size: 14px;
     font-weight: 600;
     color: #2a2a2a;
+  }
+  @media (max-width: 767px) {
+    min-width: fit-content;
   }
 `;
 const TokenLogo = styled.div``;
@@ -144,6 +179,12 @@ const LastLine = styled.div`
   margin: 0 auto;
   min-width: 311px;
   border: solid 0.5px #e3e7eb;
+  ${({ theme }) => theme === "dark" && `
+    border: solid 0.5px #3552a5;
+  `}
+  @media (max-width: 767px) {
+    min-width: fit-content;
+  }
 `;
 const CustomDropDownAddress = (props) => {
   const { price, options, onSelect } = props;
@@ -204,7 +245,7 @@ const CustomDropDownAddress = (props) => {
 
   return (
     <Container ref={mainDiv}>
-      <SelectedValueContainer onClick={onFilterClicked}>
+      <SelectedValueContainer theme={props.theme} onClick={onFilterClicked}>
         <TokenBalanceContainer>
         {isPriceVisibilty && <TokenUsdBalance >
             {currencySymbol}
@@ -226,15 +267,15 @@ const CustomDropDownAddress = (props) => {
         </div>
       </SelectedValueContainer>
       {isDropdownOpen && search.length == 0 ? (
-        <DropdownContainer containerWidth={mainDiv.current.clientWidth}>
-          <SearchBox>
+        <DropdownContainer theme={props.theme} containerWidth={mainDiv.current.clientWidth}>
+          <SearchBox theme={props.theme}>
             <img src="/images/Search.svg" />
             <input
               placeholder="Search"
               onChange={(e) => setSearch(e.target.value)}
             />
           </SearchBox>
-          <TokenHeading>
+          <TokenHeading theme={props.theme}>
             <span>XRC-20 Token ({filterXrc20.length})</span>
           </TokenHeading>
           {filterXrc20?.map((data, index) => {
@@ -287,7 +328,7 @@ const CustomDropDownAddress = (props) => {
               </a>
             );
           })}
-          <TokenHeading>
+          <TokenHeading theme={props.theme}>
             <span>XRC-721 Token ({filterXrc721.length})</span>
           </TokenHeading>
           {filterXrc721?.map((data, index) => {
@@ -338,12 +379,12 @@ const CustomDropDownAddress = (props) => {
               </a>
             );
           })}
-          <LastLine></LastLine>
+          <LastLine theme={props.theme}></LastLine>
         </DropdownContainer>
       ) : (
         isDropdownOpen && (
-          <DropdownContainer containerWidth={mainDiv.current.clientWidth}>
-            <SearchBox>
+          <DropdownContainer theme={props.theme} containerWidth={mainDiv.current.clientWidth}>
+            <SearchBox theme={props.theme}>
               <img src="/images/Search.svg" />
               <input
                 placeholder="Search"
